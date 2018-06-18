63f445e71b30161dfd97ee7ef1a9df7e6d6e01a5
Igal Roitblat <igalroitblat-MBP@MBP-Igalr.yotpo.com>
2018-06-18 19:42:12 +0300

    prime numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,41 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+	public numbers: number[] = [];
+	public filteredList: number[] = [];
+	private iterations: number = 1000;
+	private divider: number = 10;
+
+	constructor() {
+		this.populateNumbers();
+	}
+
+	public populateNumbers() {
+		for (var i:number = 1; i <= this.iterations; i++) {
+			this.numbers.push(i);
+		}
+		console.log(this.numbers);
+		this.filterDividables();
+	}
+
+	public isPrime(number:number):boolean {
+		for (var i: number = 2; i < number; i++) {
+			if(number % i === 0) return false;
+		}
+
+		return true;
+	}
+
+	public filterDividables():void {
+		for (var i: number = 1; i < this.numbers.length; i++) {
+			if(i % this.divider !== 0) {
+				this.filteredList.push(i);
+			}
+		}
+
+	}
+
+
 }
